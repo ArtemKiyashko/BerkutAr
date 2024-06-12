ce7d37121c4a808759a40241c1f0fb84c0435943
Artem Kiyashko <artem86@hotmail.co.uk>
2024-06-12 22:43:52 +0200

    Add TG app support

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import * as React from 'react';
 import './App.css';
 import Scene from './components/Scene/Scene';
 
 function App() {
+
+  React.useEffect(() => {
+    window.Telegram.WebApp.ready();
+    window.Telegram.WebApp.expand();
+  }, []);
+
   return (
     <Scene />
   );
